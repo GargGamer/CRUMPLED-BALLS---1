7d430775e9ed585c39a441a02cde32cc8ad71293
GargGamer <74178213+GargGamer@users.noreply.github.com>
2021-01-06 22:13:45 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world
+var ground
+var bin1, bin2, bin3
+var paperball
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(400,height,800,20);
+
+  bin1 = new bin(540,630,30,170)
+	bin2 = new bin(600,660,150,30)
+	bin3 = new bin(660,630,30,170)
+
+	paperball = new paper(100,630,30,30)
+
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  keyPressed()
+  
+  ground.display()
+  bin1.display()
+  bin2.display()
+  bin3.display()
+  paperball.display()
+ 
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+      Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:600,y:630})
+    }
+}
+
+
+
